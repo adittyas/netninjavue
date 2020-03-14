6d57454b79eadb3de672d6e887218f233c35d135
aditya <aditya.sandy23@gmail.com>
2020-03-14 08:57:14 +0700

    'add'

--- src/main.js
+++ src/main.js
@@ -6,6 +6,29 @@ import VueResource from "vue-resource";
 
 Vue.use(VueResource);
 
+Vue.directive("rainbow", {
+  bind(el) {
+    el.style.color =
+      "#" +
+      Math.random()
+        .toString()
+        .slice(2, 8);
+  }
+});
+
+Vue.directive("theme", {
+  bind(el, binding) {
+    if (binding.value == "wide") {
+      el.style.maxWidth = "1200px";
+    } else if (binding.value == "narrow") {
+      el.style.maxWidth = "560px";
+    }
+    if (binding.arg == "column") {
+      (el.style.background = "#ddd"), (el.style.padding = "20px");
+    }
+  }
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
